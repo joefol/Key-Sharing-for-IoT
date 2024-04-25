5f79bbe0d0804333d75b65e3b3e9dd497993f97b
Joe Folen <joe.folen@gmail.com>
2024-04-25 15:10:34 -0500

    test keys update to PI

--- src/Pi/client.py
+++ src/Pi/client.py
@@ -1,6 +1,7 @@
 import socket
 import pickle
 from Crypto.Protocol.SecretSharing import Shamir
+from Crypto.Cipher import AES
 
 HOST = '127.0.0.1'
 PORT = 65432
@@ -9,10 +10,25 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
     client_socket.connect((HOST, PORT))
 
     # Start pi
+    # 28 test keys
+
+    test_keys = []
+    test_key_shares = []
+
+    # generate 28 test keys of size 128-bit for AES encryption/decryption
+    # split each test keys into 6 shares, need 4 to reconstruct
+    for i in range(27):
+        test_keys.append(b'Sixteen byte key')
+        test_key_shares.append(Shamir.split(4, 6, test_keys[i]))
+        data = pickle.dumps(test_key_shares[i])
+        client_socket.sendall(data)
+
+    '''
     secret = 28
     print("\nSecret: ", secret)
 
     shares = Shamir.split(4, 6, secret)
-    
+
     data = pickle.dumps(shares)
     client_socket.sendall(data)
+    '''
